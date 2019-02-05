b9fa5e90e87b3d0b995171f6da43774d053ce195
rendlik <rsonroad@gmail.com>
2019-02-05 21:46:37 +0100

    first commit

---
+++ models/Store.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+const slug = require('slugs')
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name.'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{ type: Number, required: 'You must supple coordinates!' }],
+    address: {
+      type: String,
+      required: 'Must supply an address'
+    }
+  }
+})
+
+storeSchema.pre('save', function (next) {
+  if (!this.isModified('name')) {
+    next() // skip it
+    return // stop function from running
+  }
+  this.slug = slug(this.name)
+  next()
+})
+
+module.exports = mongoose.model('Store', storeSchema)
